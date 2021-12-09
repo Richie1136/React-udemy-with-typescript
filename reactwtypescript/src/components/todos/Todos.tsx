673d7a257602f9efae8c0c68f0143d5c7a71da4a
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-12-09 13:46:46 -0500

    Created a Todos component with 2 lists items and then imported it into the App.tsx file and put it into the return

---
+++ reactwtypescript/src/components/todos/Todos.tsx
@@ -0,0 +1,12 @@
+const Todos = () => {
+  return (
+    <div>
+      <ul>
+        <li>Learn React</li>
+        <li>Learn Typescript</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Todos;
